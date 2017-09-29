7cc144169d8e3cdadc53fe0b2b20b5669ec06c6d
Zacarías Monzón Santana <zacariasm@aplicaciones.atlante>
2017-09-29 09:01:27 +0100

    Nuevas componentes

---
+++ src/app/components/body.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-body',
+  templateUrl: './body.component.html'
+  /* styleUrls: ['./app.component.css'] <<< este estilo solo afecta al component se puede borrar*/
+})
+export class BodyComponent {
+  /*title = 'app';*/
+
+}
---
+++ src/app/components/header.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html'
+  /* styleUrls: ['./app.component.css'] <<< este estilo solo afecta al component se puede borrar*/
+})
+export class HeaderComponent {
+  /*title = 'app';*/
+
+}
