f54429490ed4d98237681178069f80ce55e5b820
Zacarías Monzón Santana <zacariasm@aplicaciones.atlante>
2017-09-29 14:08:55 +0100

    Modificaciones del footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  /* styleUrls: ['./app.component.css'] <<< este estilo solo afecta al component se puede borrar*/
 })
 export class AppComponent {
-  title = 'app';
+  /*title = 'app';*/
+   nombre:string="Brandon";
+   apellidos:string="Breineng";
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent }from './components/header.component';
+import { BodyComponent }from './components/body.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BodyComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
